c7479e6ad4b71d66ccb83038e04b2a1de2183690
AeroSm1th <aerosmith@example.com>
2026-03-15 14:32:21 +0800

    test: 测试 pre-commit hook

---
+++ src/test-lint.js
@@ -0,0 +1,7 @@
+// 这是一个测试文件，用于验证 pre-commit hook
+const test = "double quotes should be single";
+const unused = 'this variable is unused';
+
+console.log('This should trigger a warning');
+
+export default test;
